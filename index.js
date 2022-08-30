9d3bdb7a6679f898dae5aa0b78ee1c2bc437ef4d
Andrei Costea <andrei@mindfullab.co>
2022-08-30 16:09:42 +0300

    dunno what i changed

--- index.js
+++ index.js
@@ -5,39 +5,43 @@ const shuffle = require('./shuffle');
 
 const elo = new EloRank(15);
 
+function calculateEloChange(inputData) {
+  const drivers = Object.keys(inputData);
+  // generate 1v1 combinations to calculate elo change
+  const combinations = Combinatorics.Combination.of(drivers, 2).toArray();
+  
+  combinations.map(combination => {
+  
+    const player1Win = elo.getExpected(inputData[combination[0]].rating, inputData[combination[1]].rating);
+    const player2Win = elo.getExpected(inputData[combination[1]].rating, inputData[combination[0]].rating);
+  
+    inputData[combination[0]].rating = elo.updateRating(player1Win, 1, inputData[combination[0]].rating);
+    inputData[combination[1]].rating = elo.updateRating(player2Win, 0, inputData[combination[1]].rating);
+  
+  })
+  return inputData
+}
+
+function testEloCalculation(inputData) {
+  const drivers = shuffle(inputData);
+  const raceOutput = calculateEloChange(drivers)
+  
+  const sortedData = raceOutput.sort((a, b) => {
+    return b.rating - a.rating;
+  })
+
+  return sortedData;
+}
+
 console.log("INPUT DATA:")
 console.log(testData)
 console.log("==========================================================")
 console.log("")
 
-// randomize the data to get fake race results
-const drivers = shuffle(testData);
+let output = [];
 
-console.log("RACE RESULTS:")
-console.log(testData)
-console.log("==========================================================")
-console.log("")
+for (let i = 0; i < 10000; i++) {
+  output = testEloCalculation(testData);
+}
 
-// generate 1v1 combinations to calculate elo change
-const combinations = Combinatorics.Combination.of(drivers, 2).toArray();
-
-combinations.map(combination => {
-
-  const player1Win = elo.getExpected(combination[0].rating, combination[1].rating);
-  const player2Win = elo.getExpected(combination[1].rating, combination[0].rating);
-
-  combination[0].rating = elo.updateRating(player1Win, 1, combination[0].rating);
-  combination[1].rating = elo.updateRating(player2Win, 0, combination[1].rating);
-
-})
-
-// sort testdata by rating
-
-const sortedData = testData.sort((a, b) => {
-  return b.rating - a.rating;
-})
-
-console.log("FINAL ELO RESULTS:")
-console.log(sortedData)
-console.log("==========================================================")
-console.log("")
+console.log(output)

af352068d1a45200ee7c68ca955d2886aea199ba
Andrei Costea <andrei@mindfullab.co>
2022-08-30 15:52:56 +0300

    use original objects

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const Combinatorics = require('js-combinatorics');
 const EloRank = require('elo-rank');
 const testData = require('./testData');
+const shuffle = require('./shuffle');
 
 const elo = new EloRank(15);
 
 // generate combinations of drivers from testData
-const drivers = Object.keys(testData);
+const drivers = shuffle(testData);
+
+console.log(drivers)
+
 const combinations = Combinatorics.Combination.of(drivers, 2).toArray();
 
 console.log(combinations)
 
 combinations.map(combination => {
 
-  const player1Win = elo.getExpected(testData[combination[0]].rating, testData[combination[1]].rating);
-  const player2Win = elo.getExpected(testData[combination[1]].rating, testData[combination[0]].rating);
+  const player1Win = elo.getExpected(combination[0].rating, combination[1].rating);
+  const player2Win = elo.getExpected(combination[1].rating, combination[0].rating);
 
-  testData[combination[0]].rating = elo.updateRating(player1Win, 1, testData[combination[0]].rating);
-  testData[combination[1]].rating = elo.updateRating(player2Win, 0, testData[combination[1]].rating);
-
-  console.log(testData[combination[0]].rating, testData[combination[1]].rating)
+  combination[0].rating = elo.updateRating(player1Win, 1, combination[0].rating);
+  combination[1].rating = elo.updateRating(player2Win, 0, combination[1].rating);
 
 })
+
+console.log(testData);
